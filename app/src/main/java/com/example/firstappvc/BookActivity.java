1bfe4f37a52bcce3dd915fe9f9097de28ba749f7
SergioSimental <sergios3456@gmail.com>
2021-09-16 08:33:24 -0700

    Book api service

---
+++ app/src/main/java/com/example/firstappvc/BookActivity.java
@@ -0,0 +1,29 @@
+package com.example.firstappvc;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BookActivity extends AppCompatActivity {
+    EditText mBookInput;
+    TextView mTitleText, mAuthorText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+        mBookInput = findViewById(R.id.bookInput);
+        mTitleText = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.authorText);
+    }
+
+
+    public void searchBooksAPI(View view) {
+        String bookName = mBookInput.getText().toString();
+        FetchBookTask fetchBookTask = new FetchBookTask(mTitleText, mAuthorText);
+        fetchBookTask.execute(bookName);
+    }
+}
---
+++ app/src/main/java/com/example/firstappvc/FetchBookTask.java
@@ -0,0 +1,61 @@
+package com.example.firstappvc;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+//ip type is bookname -- string, op type is a jsonString
+
+public class FetchBookTask extends AsyncTask<String, Void, String> {
+    TextView titleTv, authorTv;
+
+
+    public FetchBookTask(TextView mTitleText, TextView mAuthorText){
+        titleTv = mTitleText;
+        authorTv = mAuthorText;
+    }
+
+
+    @Override
+    protected String doInBackground(String... bookName){
+        return NetworkUtils.getBookInfo(bookName[0]);
+    }
+
+    @Override protected void onPostExecute(String jsonString) {
+        super.onPostExecute(jsonString);
+
+        try {
+            JSONObject rootJsonObject = new JSONObject(jsonString);
+            JSONArray itemsArray = rootJsonObject.getJSONArray("items");
+            for (int i = 0; i < itemsArray.length(); i++) {
+                JSONObject book = itemsArray.getJSONObject(i);
+                String title = null;
+                String authors = null;
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                if (title != null && authors != null) {
+                    titleTv.setText(title);
+                    authorTv.setText(authors);
+                    return;
+                }
+
+                titleTv.setText("No Results Found");
+                authorTv.setText("");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
