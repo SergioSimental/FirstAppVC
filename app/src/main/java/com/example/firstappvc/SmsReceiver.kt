641257593943c8af4b73f5b92380d64ada29f2bf
SergioSimental <sergios3456@gmail.com>
2021-09-16 11:08:15 -0700

    Sms receiver

--- app/src/main/java/com/example/firstappvc/SmsReceiver.kt
+++ app/src/main/java/com/example/firstappvc/SmsReceiver.kt
@@ -1,4 +1,14 @@
 package com.example.firstappvc
 
-class SmsReceiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class SmsReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent){
+        //This method is called when the Broadcast Receiver is receiving an Intent broadcast.
+        Toast.makeText(context, "abdul you have an sms sergio", Toast.LENGTH_SHORT).show()
+    }
 }
